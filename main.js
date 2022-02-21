2e11def026894908fc4c57d1cabd74fd76dfde45
jieyuzhou42 <jieyuz@andrew.cmu.edu>
2022-02-21 11:17:48 -0500

    change2

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+function addListItem() {
+    let list=document.getElementById("grocery-list");
+    let itemInput=document.getElementById("text-input");
+    let newItem=document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
